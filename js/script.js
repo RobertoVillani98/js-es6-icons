dc9d7d77f2591205cceda1ffcfcf54d85e9fc4c8
RobertoVillani <robertovillani58@gmail.com>
2021-11-23 19:01:44 +0100

    fix milestone 3

--- js/script.js
+++ js/script.js
@@ -50,12 +50,19 @@ function filterElements (){
     let value = this.value;
 	
     
-    // console.log("value ---------> ",value);
     
     //filtra gli elementi selezionati in base al tipo scelto e li mette nell'arrray
-    let newCardArr = card.filter((item)=> item.type===value);
-    //fa vedere i nuovi elementi filtrati
-    costruzioneCard(newCardArr);
+
+    let newCardArr = card
+
+    //se seleziono all il new Card arr sarà uguale all'array di partenza
+    //altrimenti devo filtrare in base al type
+    if(value !== 'all'){
+        newCardArr = card.filter((item)=> {
+        if(item.type === value) return item
+    });
 }
 
+costruzioneCard(newCardArr)
 
+}
